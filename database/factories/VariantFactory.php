ad76b7bfc7ffe7595ff5f7f5d168a240ed21fb30
Saddam Hossen <saddamhshovon@gmail.com>
2024-06-03 11:14:23 +0600

    add: model, migration, seeder, factory

---
+++ database/factories/VariantFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Support\Arr;
+use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Variant>
+ */
+class VariantFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'color' => fake()->colorName(),
+            'specification' => fake()->sentences(asText: true),
+            'size' => Arr::random(['small', 'medium', 'large']),
+        ];
+    }
+}
